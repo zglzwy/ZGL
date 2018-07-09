7cca11fdd1b723cb7614e15a94233b2ea7c12dc1
zgl <2668853024@qq.com>
2018-07-09 17:14:56 +0800

    实体类创建，学生类，课程类，成绩类 读取数据

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-    publc static void main(String[] args){
-        System.out.prinln("hello");
+    public static void main(String[] args){
+        System.out.println("hello");
     }
 }
